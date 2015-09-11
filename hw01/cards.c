6be2d4ff5fc8b3f914e76fe18b63b68861a4eff5
flymperopoulos <filippos.lymperopoulos@students.olin.edu>
2015-09-11 01:19:12 -0400

    FIX: extra flag checking and docstrings

--- hw01/cards.c
+++ hw01/cards.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Prompts the user for input and puts the reply in the given buffer.
+
+   User input is truncated to the first two characters.
+
+   card_name[]: buffer-array where result is stored 
+*/
 void read_card(char card_name[]){
 	puts("Enter the card_name: ");
 	scanf("%2s", card_name);
 }
 
+/* Changes counter based on value of val variable.
+
+   val: integer where value is stored
+   count: integer returned
+*/
 int counter(int *val, int *count){
 	if ((*val>2) && (*val<7)){
 		*count = *count + 1;
@@ -15,10 +26,15 @@ int counter(int *val, int *count){
 	return *count;
 }
 
+/* Checks user's input and adjusts val variable accordingly.
+
+   card_name[]: buffer-array where result is stored
+   count: integer passed to counter method 
+*/
 void choises(char card_name[], int *count){
 	while (card_name[0]!='X'){
-		read_card(card_name);
 		int val = 0;
+		read_card(card_name);
 		switch (card_name[0]){
 			case 'K':
 			case 'Q':
@@ -42,6 +58,7 @@ void choises(char card_name[], int *count){
 	}
 }
 
+/* Main file method */
 int main(){
 	char card_name[3];
 	int count = 0;
